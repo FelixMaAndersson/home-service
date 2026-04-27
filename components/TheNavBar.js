d5dc50ff22878cd6eaae10af5892ad3f827be88e
Felix Andersson <felix.andersson@gmail.com>
2026-04-27 15:43:38 +0200

    done with NavBar

--- components/TheNavBar.js
+++ components/TheNavBar.js
@@ -12,25 +12,23 @@ export default {
         }
     },
     template: `
-        <nav class="main-navbar">
-            <div class="nav-left">
-                <div class="logo-placeholder">
-                    <img src="assets/logo.png" class="icon" >
-                </div>
-            </div>
+<nav class="main-navbar">
+    <div class="nav-left">
+        <img src="assets/logo.png" class="icon">
 
-            <ul class="nav-menu">
-                <li v-for="item in menuItems" 
-                    :key="item.id"
-                    :class="{ active: activeTab === item.id }"
-                    @click="activeTab = item.id">
-                    {{ item.name }}
-                </li>
-            </ul>
+        <ul class="nav-menu">
+            <li v-for="item in menuItems" 
+                :key="item.id"
+                :class="{ active: activeTab === item.id }"
+                @click="activeTab = item.id">
+                {{ item.name }}
+            </li>
+        </ul>
+    </div>
 
-            <div class="nav-right">
-                <a href="#" class="logout-link">LOGGA UT</a>
-            </div>
-        </nav>
+    <div class="nav-right">
+        <a href="#" class="logout-link">LOGGA UT</a>
+    </div>
+</nav>
     `
 };
